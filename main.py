f7a1752c613e7e5f9efed90e60298f4334a6f94c
CodeSasuke <siddhantbharadwaajcs@gmail.com>
2026-05-17 18:36:45 +0530

    Add function to change bullet style in documents

--- main.py
+++ main.py
@@ -89,3 +89,21 @@ def file_type_getter(
     return lambda input_file: map.get(input_file, "Unknown")
 
     
+from collections.abc import Iterator
+def change_bullet_style(document: str) -> str:
+    lines_list = document.split("\n")
+
+    lines_changed: Iterator[str] = map(convert_line,lines_list)
+    rejoined_docs = '\n'.join(lines_changed)
+    return rejoined_docs
+
+
+# Don't edit below this line
+
+
+def convert_line(line: str) -> str:
+    old_bullet = "-"
+    new_bullet = "*"
+    if len(line) > 0 and line[0] == old_bullet:
+        return new_bullet + line[1:]
+    return line
